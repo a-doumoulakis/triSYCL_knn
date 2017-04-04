fce2b603d6671379759e8a036b43482c6c2f795e
Ronan Keryell <ronan.keryell@xilinx.com>
2017-04-04 10:12:17 +0100

    Modernize presentation

--- knn_trisycl.cpp
+++ knn_trisycl.cpp
@@ -27,33 +27,33 @@ std::vector<Img> validation_set;
 
 buffer<int> get_buffer(const std::vector<Img>& imgs) {
   std::vector<int> res;
-  for(Img elem : imgs){
+  for (Img elem : imgs) {
     res.insert(res.end(), std::begin(elem.pixels), std::end(elem.pixels));
   }
-  return buffer<int>{std::begin(res), std::end(res)};
+  return buffer<int> { std::begin(res), std::end(res) };
 }
 
 std::vector<Img> slurp_file(const std::string& name) {
-  std::ifstream infile(name, std::ifstream::in);
+  std::ifstream infile { name, std::ifstream::in };
   std::cout << "Loading " << name << std::endl;
   std::string line, token;
   std::vector<Img> res;
   bool fst_1 = true;
   while (std::getline(infile, line)) {
-    if(fst_1){
+    if (fst_1) {
       fst_1 = false;
       continue;
     }
     Img img;
-    std::istringstream iss(line);
+    std::istringstream iss { line };
     bool fst = true;
     int index = 0;
-    while(std::getline(iss, token, ',')) {
-      if(fst){
+    while (std::getline(iss, token, ',')) {
+      if (fst) {
         img.label = std::stoi(token);
         fst = false;
       }
-      else{
+      else {
         img.pixels[index] = std::stoi(token);
         index++;
       }
@@ -74,11 +74,11 @@ int compute(buffer<int>& training, const Img& img, queue& q) {
         auto train = training.get_access<access::mode::read>(cgh);
         auto ka = A.get_access<access::mode::read>(cgh);
         auto kb = B.get_access<access::mode::write>(cgh);
-        cgh.parallel_for(range<1>{5000}, [=] (id<1> index){
+        cgh.parallel_for(range<1> { 5000 }, [=] (id<1> index){
             int diff, toAdd;
-            if(index < 5000){
+            if (index < 5000) {
               diff = 0;
-              for(int i = 0; i < 784; i++){
+              for (int i = 0; i < 784; i++) {
                 toAdd = ka[i] - train[index[0] * 784 + i];
                 diff += toAdd * toAdd;
               }
@@ -90,14 +90,14 @@ int compute(buffer<int>& training, const Img& img, queue& q) {
 
   int index = 0;
   double square = std::sqrt(res[0]);
-  for(unsigned i = 1; i < training_set_size; i++){
+  for (unsigned i = 1; i < training_set_size; i++) {
     double tmp = std::sqrt(res[i]);
-    if(tmp < square){
+    if (tmp < square) {
       index = i;
       square = tmp;
     }
   }
-  if(training_set[index].label == img.label) return 1;
+  if (training_set[index].label == img.label) return 1;
   return 0;
 }
 
@@ -110,12 +110,14 @@ int main(int argc, char* argv[]) {
   queue q;
 
   boost::posix_time::ptime mst1 = boost::posix_time::microsec_clock::local_time(); 
- 
-  for(Img img : validation_set) correct += compute(training_buffer, img, q);
+
+  for (Img img : validation_set)
+    correct += compute(training_buffer, img, q);
+
   boost::posix_time::ptime mst2 = boost::posix_time::microsec_clock::local_time();
   boost::posix_time::time_duration msdiff = mst2 - mst1;
   std::cout << (msdiff.total_milliseconds() / 500.0) << std::endl;
-  
+
   std::cout << "\nResult : " << ((correct / 500.0) * 100.0) << "%"
             << " (" << correct << ")"
             << std::endl;
